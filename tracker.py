0714204e6e458b9020c3cacaf2f27c77a91f8502
Alex de Mulder <alexdemulder@gmail.com>
2016-11-24 14:04:10 +0100

    tracker script

---
+++ tracker.py
@@ -0,0 +1,21 @@
+import time;
+
+print "Who are you?"
+username = raw_input("line 1: ")
+
+filename = username + ".txt"
+
+print "Opening the file..."
+target = open(filename, 'a')
+
+lastTime = time.time()
+while 1:
+    raw_input("Are you done yet?? ")
+    currentTime = time.time()
+    dt = str(currentTime - lastTime)
+    target.write("t:" + str(time.time()) + "," + str(time.asctime( time.localtime(time.time()) )) + "," + dt + "\n")
+
+    print "Took you long enough..." + dt
+
+    lastTime = time.time()
+
